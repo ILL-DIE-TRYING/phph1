f2b4bbb638a35c4956732d8d92b344f68f5834f3
ILL-DIE-TRYING <92170977+ILL-DIE-TRYING@users.noreply.github.com>
2022-05-29 07:31:53 -0600

    Method file for hmyv2_getCurrentTransactionErrorSink was missing. Removed outdate help file

---
+++ methods/hmyv2_getCurrentTransactionErrorSink.php
@@ -0,0 +1,68 @@
+<?php
+/**
+* Method file for hmyv2_getCurrentTransactionErrorSink() in the phph1.php class file
+*/
+
+// There is no input so no validation required
+$phph1->set_validinput(1);
+
+// Get the block number dataset
+$hmyv2_data = $phph1->hmyv2_getCurrentTransactionErrorSink();
+
+require_once('inc/errors.php');
+
+/**
+* Check if this is a RPC call
+* If not show the html output of the method explorer
+*/
+if($phph1->get_rpcstatus() == 0){
+?>
+
+	<div class="info_container" >
+		<div class="infoRow">
+			<button type="button" class="collapsibleInfo"><?=$phph1->get_currentmethod()?> :: Params/Returns</button>
+			<div id="infoContent" class="infoContent">
+			
+				<h3 class="infoHeader">Description</h3>
+				<ul class="infoObjects" >
+					<li class="infoObjectNoBul">
+						<div>
+							<p>Gets a list of failed transactions currently in the transaction errors sink.</p>
+							<p>There may be more information in the <a href="./doc/classes/phph1.html#method_hmyv2_getCurrentTransactionErrorSink">PHPH1 Class Documentation</a>.</p>
+						</div>
+					</li>
+				</ul>
+			
+				<h3 class="infoHeader">Parameters</h3>
+				<ul class="infoObjects" >
+					<li class="infoObjectNoBul"><h4>No Parameters Required</h4></li>
+				</ul>
+				
+				<h3 class="infoHeader">Returns</h3>
+				<ul class="infoObjects">
+					
+					<li class="infoObjectNoBul"><span>Array</span> of <span>Object</span>:</li>
+					
+					<li><div class="ioobjectWrap"><span >error-message</span> - <span>String</span>:</div> 
+					<div class="iodefWrap">Reason for transaction rejection</div></li>
+					
+					<li><div class="ioobjectWrap"><span >time-at-rejection</span> - <span>Number</span>:</div> 
+					<div class="iodefWrap">Unix time when the transaction was rejected from the pool</div></li>
+					
+					<li><div class="ioobjectWrap"><span >tx-hash-id</span> - <span>String</span>:</div> 
+					<div class="iodefWrap">Transaction hash</div></li>
+
+				</ul>
+			</div>
+		</div>
+	</div>
+
+<?php
+/**
+* ends the rpc call check
+*/
+}
+
+require_once('inc/output.php');
+?>
+
